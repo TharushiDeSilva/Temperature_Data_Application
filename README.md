6cf1e0584aaff6a058a2af6c0ad98603790f8996
Tharushi De Silva <tharushi.de.silva@ifsworld.com>
2017-09-20 09:11:32 +0530

    Sensor data is written into a properties file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 mvn clean install
 place the temperature-data-application.war in apache tomcat--> webapps
 Http://localhost:8080/temperature-data-application
+Writes sensor data to a properties file. the output stream writes only one set of data at a time. this should be handled.
+Since the next step is to write data to a sql database, the previous issue was not property handled.
--- src/main/java/com/tharushi/temperature/data/controller/WeatherDataHandler.java
+++ src/main/java/com/tharushi/temperature/data/controller/WeatherDataHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Properties;
 
 @WebServlet ("/weather-data")
 public class WeatherDataHandler extends HttpServlet{
@@ -45,11 +46,57 @@ public class WeatherDataHandler extends HttpServlet{
         request.setAttribute("humidity", Double.toString(humidity));
         request.setAttribute("heatIndex", Double.toString(heatIndex));
         request.getRequestDispatcher("/ftl/weatherData.ftl").forward(request, response);
+        //System.out.println(readLogData("19-09-2017//16-54-00"));
+        writeLogData(date,time, sensorID, temperature, humidity, heatIndex);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
+    }
+
+
+    // the code below is connected to writing the sensor data into a properties file. ---------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    private String createKey(String date, String time){
+        String key = date+"//"+time;
+        return key;
+    }
+    private String createValue(int sensorID, Double temperature, Double humidity, Double heatIndex){
+        String value = sensorID+"//"+temperature+"//"+humidity+"//"+heatIndex;
+        return value;
+    }
+    protected String readLogData(String key) {
+        Properties prop = new Properties();
+        InputStream input = null;
+        String value = "not defined";
+        try{
+            input = Thread.currentThread().getContextClassLoader().getResourceAsStream("weather-data.properties");
+            prop.load(input);
+            value = (String) prop.getProperty(key);
+            input.close();
+        }catch(IOException io){
+            io.printStackTrace();
+        }
+        return value;
+    }
+    protected void writeLogData(String date, String time, int sensorID, Double temperature, Double humidity, Double heatIndex){
+        String key = createKey(date, time);
+        String value = createValue(sensorID, temperature, humidity, heatIndex);
+        Properties prop = new Properties();
+        InputStream input = null;
+        OutputStream output = null;
+        try{
+            input = Thread.currentThread().getContextClassLoader().getResourceAsStream("weather-data.properties");
+            prop.load(input);
+            output = new FileOutputStream("D:/Temperature_Data_Application/src/main/resources/weather-data.properties");
+            prop.setProperty(key, value);
+            prop.store(output, null);
+            input.close();
+            output.close();
+        }catch(IOException io){
+            io.printStackTrace();
+        }
     }
 
 }
