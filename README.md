b482e2929ac2cb1163aaefe8dccb1beae0881465
Tharushi De Silva <tharushi.de.silva@ifsworld.com>
2017-09-21 16:00:46 +0530

    Properties file read/write, POST requests resolved

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 mvn clean install
 place the temperature-data-application.war in apache tomcat--> webapps
 Http://localhost:8080/temperature-data-application
-Writes sensor data to a properties file. the output stream writes only one set of data at a time.
-This happens because the properties file is read from the server and written to the local machine.
+
+DHT11 data is handled by the POST method.
+Sensor data are written into weatherData.properties that resides within WEB-INF folder. This file can now be read and write.
--- src/main/java/com/tharushi/temperature/data/controller/WeatherDataHandler.java
+++ src/main/java/com/tharushi/temperature/data/controller/WeatherDataHandler.java
@@ -19,25 +19,6 @@ public class WeatherDataHandler extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //sending temperature data to this servlet as GET requests and they are processes within this module.
-        if(request.getParameterMap().containsKey("sensorID")) {
-            this.sensorID = Integer.parseInt(request.getParameter("sensorID"));
-        }
-        if(request.getParameterMap().containsKey("date")) {
-            this.date = request.getParameter("date");
-        }
-        if(request.getParameterMap().containsKey("time")) {
-            this.time = request.getParameter("time");
-        }
-        if(request.getParameterMap().containsKey("temperature")) {
-            this.temperature = Double.parseDouble(request.getParameter("temperature"));
-        }
-        if(request.getParameterMap().containsKey("humidity")) {
-            this.humidity = Double.parseDouble(request.getParameter("humidity"));
-        }
-        if(request.getParameterMap().containsKey("heatIndex")) {
-            this.heatIndex = Double.parseDouble(request.getParameter("heatIndex"));
-        }
 
         request.setAttribute("sensorID", Integer.toString(sensorID));
         request.setAttribute("date", date);
@@ -46,12 +27,13 @@ public class WeatherDataHandler extends HttpServlet{
         request.setAttribute("humidity", Double.toString(humidity));
         request.setAttribute("heatIndex", Double.toString(heatIndex));
         request.getRequestDispatcher("/ftl/weatherData.ftl").forward(request, response);
-        //System.out.println(readLogData("19-09-2017//16-54-00"));
-        writeLogData(date,time, sensorID, temperature, humidity, heatIndex);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        setParameters(request);
+        writeLogData(date,time, sensorID, temperature, humidity, heatIndex);
+
 
     }
 
@@ -62,41 +44,90 @@ public class WeatherDataHandler extends HttpServlet{
         String key = date+"//"+time;
         return key;
     }
+
     private String createValue(int sensorID, Double temperature, Double humidity, Double heatIndex){
         String value = sensorID+"//"+temperature+"//"+humidity+"//"+heatIndex;
         return value;
     }
+
     protected String readLogData(String key) {
-        Properties prop = new Properties();
-        InputStream input = null;
-        String value = "not defined";
+        String value = "";
+        Properties properties = new Properties();
+        FileInputStream input = null;
         try{
-            input = Thread.currentThread().getContextClassLoader().getResourceAsStream("weather-data.properties");
-            prop.load(input);
-            value = (String) prop.getProperty(key);
+            input = new FileInputStream(getServletContext().getRealPath("/WEB-INF/classes/weatherData.properties"));
+            properties.load(input);
+            value = (String) properties.getProperty(key);
             input.close();
-        }catch(IOException io){
-            io.printStackTrace();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
         }
         return value;
     }
+
     protected void writeLogData(String date, String time, int sensorID, Double temperature, Double humidity, Double heatIndex){
         String key = createKey(date, time);
         String value = createValue(sensorID, temperature, humidity, heatIndex);
-        Properties prop = new Properties();
-        InputStream input = null;
-        OutputStream output = null;
+        Properties properties = new Properties();
+        FileOutputStream fileOut = null;
+        FileInputStream fileIn = null;
         try{
-            input = Thread.currentThread().getContextClassLoader().getResourceAsStream("weather-data.properties");
-            prop.load(input);
-            output = new FileOutputStream("D:/Temperature_Data_Application/src/main/resources/weather-data.properties");
-            prop.setProperty(key, value);
-            prop.store(output, null);
-            input.close();
-            output.close();
-        }catch(IOException io){
-            io.printStackTrace();
+            fileIn = new FileInputStream(getServletContext().getRealPath("/WEB-INF/classes/weatherData.properties"));
+            properties.load(fileIn);
+            fileOut = new FileOutputStream(getServletContext().getRealPath("/WEB-INF/classes/weatherData.properties"));
+            properties.setProperty(key, value);
+            properties.store(fileOut, null);
+            fileOut.close();
+            fileIn.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
         }
+    }
+
+    protected String getBody(HttpServletRequest request) throws IOException{
+        String body = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+        try{
+            InputStream inputStream = request.getInputStream();
+            if(inputStream != null){
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while((bytesRead = bufferedReader.read(charBuffer))>0){
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            }else{
+                stringBuilder.append("");
+            }
+        }catch (IOException ex){
+            throw ex;
+        }finally {
+            if(bufferedReader != null){
+                try{
+                    bufferedReader.close();
+                }catch (IOException ex){
+                    throw ex;
+                }
+            }
+        }
+        body = stringBuilder.toString();
+        return body;
+    }
+
+    protected void setParameters(HttpServletRequest request) throws IOException{
+        String[] paramArray = getBody(request).split("\"");
+        this.sensorID = Integer.parseInt(paramArray[3]);
+        this.date = paramArray[7];
+        this.time = paramArray[11];
+        this.temperature = Double.parseDouble(paramArray[15]);
+        this.humidity = Double.parseDouble(paramArray[19]);
+        this.heatIndex = Double.parseDouble(paramArray[23]);
+
     }
 
 }
